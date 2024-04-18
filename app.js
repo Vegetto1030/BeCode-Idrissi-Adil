918f1d34555c5520590a8be8127c2eec5b6fde20
Vegetto1030 <adil.idrissi.hr@gmail.com>
2024-04-18 15:47:03 +0200

    Exercices already done

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-var a = "3";
-var b = "8";
+/*Exercice 06.b
 
-/***********Do not change the code above 👆*******/
-//Write your code here :
-var c = a;
-var a = b;
-var b = c;
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
 
-/***********Do not change the code below 👇*******/
+// YOUR CODE BELOW THIS LINE:
+console.log(message.toUpperCase().trim());
+let finalMessage = message.toUpperCase().trim();
+let lastLetter = finalMessage.charAt(finalMessage.length-1);
+console.log(lastLetter);
 
-console.log("a is " + a);
-console.log("b is " + b);
+//
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+const facialHair = word.slice(5, 10).replace("o", "e");
+console.log(facialHair);
+
+
+//Exercice 7
+
+let num1 = Math.random().toFixed(1) * 10;
+let num2 = Math.random().toFixed(1) * 10;
+let num3 = Math.random().toFixed(1) * 10;
+
+console.log(num1, num2, num3);
+
+let max = Math.max(num1, num2, num3);
+console.log(max);
+
+
+
+// Exercice 8
+const name = prompt("What is your name?");
+const age = prompt("How old are you ?");
+const live = prompt("Where do you live ?");
+
+alert(`Welcome ${name}, living in ${live}, aged of ${age} !`);
+
+//
+
+let basePrice = prompt("What's the price without the vat ?")
+basePrice = parseInt(basePrice);
+let vat = basePrice *21/100;
+let priceWithVat = basePrice + vat;
+alert(`The price with the vat is ${priceWithVat} € !`);
+
+*/
+

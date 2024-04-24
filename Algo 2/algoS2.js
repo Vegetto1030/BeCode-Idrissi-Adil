f27ac77a2fd11e39c155da0adec3ef5f80789e41
Vegetto1030 <adil.idrissi.hr@gmail.com>
2024-04-24 19:08:44 +0200

    Debut des exercices

--- Algo 2/algoS2.js
+++ Algo 2/algoS2.js
@@ -1,6 +1,17 @@
 //Exercice 0 
-
+/*
 const countTrue = (array) => array.reduce((counter, valeur) => valeur == true ? counter + 1 : counter, 0);
 console.log(countTrue([true, false, false, true, false])); 
 console.log(countTrue([false, false, false, false])); 
 console.log(countTrue([]));
+
+
+//Exercice 1
+
+const possibleBonus = (a, b) => b - a >= 1 && b - a <= 6 ? true : false;
+console.log(possibleBonus(3, 7));
+console.log(possibleBonus(1, 9));
+console.log(possibleBonus(5, 3));
+
+ */
+
